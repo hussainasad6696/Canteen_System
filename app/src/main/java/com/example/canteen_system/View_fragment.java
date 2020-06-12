257c0617284ef84692052e989706309ede241755
owner <f2016266054@umt.edu.pk>
2020-06-12 21:41:39 +0500

    delete

--- app/src/main/java/com/example/canteen_system/View_fragment.java
+++ app/src/main/java/com/example/canteen_system/View_fragment.java
@@ -1,5 +1,9 @@
 package com.example.canteen_system;
 
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -12,7 +16,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
@@ -21,6 +27,8 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -28,7 +36,9 @@ import java.util.Objects;
 
 public class View_fragment extends Fragment {
 
-     String data_key;
+     ArrayList data_key;
+    DatabaseReference ref ,reference;
+    SearchView searchView;
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,8 +46,9 @@ public class View_fragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_view_fragment, container, false);
         final ListView listView = view.findViewById(R.id.list_view);
+        data_key = new ArrayList<String>();
         final ArrayList<canteen_detail> list = new ArrayList<>();
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Canteens");
+         ref = FirebaseDatabase.getInstance().getReference().child("Canteens");
         final listView_adapter adapter = new listView_adapter(Objects.requireNonNull(getActivity()),R.layout.canteen_detail_container,list);
         listView.setAdapter(adapter);
 
@@ -47,6 +58,7 @@ public class View_fragment extends Fragment {
                 final canteen_detail canteenDetail = dataSnapshot.getValue(canteen_detail.class);
                 list.add(canteenDetail);
                 adapter.notifyDataSetChanged();
+                data_key.add(dataSnapshot.getKey());
             }
 
             @Override
@@ -69,12 +81,69 @@ public class View_fragment extends Fragment {
 
             }
         });
+
+        searchView = view.findViewById(R.id.search);
+        view.findViewById(R.id.search_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String query_search = searchView.getQuery().toString();
+                deleteData(query_search);
+            }
+        });
+        reference = FirebaseDatabase.getInstance().getReference();
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                
+                final String key_id = (String) data_key.get(position);
+                Toast.makeText(getContext(),""+key_id,Toast.LENGTH_SHORT).show();
+                /*AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Delete!")
+                        .setMessage("Are you sure you want to Delete")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                    }
+                });*/
+                final Query query;
+                query = reference.child("Canteens").orderByChild(key_id);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String key = dataSnapshot.getKey();
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
             }
         });
         return view;
+    }
+
+    private void deleteData(String query_search) {
+        Query deleteQuery = ref.orderByChild("management_detail").equalTo(query_search);
+        deleteQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot deleteSnap : dataSnapshot.getChildren())
+                {
+                    deleteSnap.getRef().removeValue();
+                }
+                Toast.makeText(getContext(),"Data Deleted",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getContext(),""+databaseError,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
