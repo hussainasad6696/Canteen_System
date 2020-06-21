d3fd43d0d1c769d66e083d6fe1e4923289a964f3
owner <f2016266054@umt.edu.pk>
2020-06-21 15:33:56 +0500

    completed

--- app/src/main/java/com/example/canteen_system/add_student.java
+++ app/src/main/java/com/example/canteen_system/add_student.java
@@ -13,18 +13,21 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class add_student extends Fragment {
     DatabaseReference databaseReference;
-
+    FirebaseAuth auth;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_student, container, false);
         databaseReference = FirebaseDatabase.getInstance().getReference("Students_admin_part");
+        auth = FirebaseAuth.getInstance();
        final EditText Fname = view.findViewById(R.id.fname);
         final EditText semester = view.findViewById(R.id.semester);
         final EditText mobile = view.findViewById(R.id.mobile_no_student);
@@ -34,9 +37,9 @@ public class add_student extends Fragment {
         view.findViewById(R.id.create_student).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String fullname =  Fname.getText().toString();
+                final String fullname =  Fname.getText().toString();
                 String Semester = semester.getText().toString();
-                String Mobile = mobile.getText().toString();
+                final String Mobile = mobile.getText().toString();
                 String DOB =  dob.getText().toString();
                 String ADDRESS =  address.getText().toString();
                 students_detail studentsDetail = new students_detail(fullname,Semester,ADDRESS,Mobile,DOB);
@@ -46,6 +49,18 @@ public class add_student extends Fragment {
                         @Override
                         public void onSuccess(Void aVoid) {
                             Toast.makeText(getContext(),"Student Added",Toast.LENGTH_SHORT).show();
+                            String mail = fullname+"@gmail.com";
+                            auth.createUserWithEmailAndPassword(mail,Mobile).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                @Override
+                                public void onSuccess(AuthResult authResult) {
+                                    Toast.makeText(getContext(),"User "+fullname+" account created",Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(getContext(),""+e,Toast.LENGTH_SHORT).show();
+                                }
+                            });
                             Fname.setText("");
                             semester.setText("");
                             mobile.setText("");
